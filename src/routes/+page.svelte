7439eda88f37b1b3cee8767b466acb6e10ee506c
Loïc Butor <butorloic90@gmail.com>
2024-11-30 12:42:41 +0100

    feat(components): LangSwitch
    Added a selector for the language, currently we have english and french.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import * as m from "$lib/paraglide/messages.js"
+    import LangSwitch from "$lib/components/LangSwitch.svelte";
 </script>
 
+<svelte:head>
+	<title>Selfinsorter</title>
+</svelte:head>
+
 <h1>{m.hello_world({name: "Loïc"})}</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<LangSwitch />
